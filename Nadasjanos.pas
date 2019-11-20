e38b112c7063dca43f0b45ea92e36e0eab3d94ea
Your Name <you@example.com>
2019-11-20 10:16:05 +0100

    NadasJanos

---
+++ Nadasjanos.pas
@@ -0,0 +1,37 @@
+program Menu
+;
+
+uses
+  foodunit, dos;
+
+var 
+  i: integer;
+  foods: foodunit.Foods;
+
+procedure httpHeader();
+begin
+  writeln('Content-type: application/json');
+  writeln('');
+end;
+
+begin
+  addFood('valami', 100);
+  
+  foods := getAllFood();
+
+  if ( GetEnv('HTTP') <> '' ) then
+    httpHeader();
+
+  writeln('{ "foods": [');
+  for i:= 0 to Length(foods) do
+  begin
+      if ( foods[i].name <> '' ) then
+      begin
+        if ( i > 0 ) then
+          write(',');
+        foods[i].toJson();
+      end;
+  end;
+  writeln(']}');
+  
+end.
